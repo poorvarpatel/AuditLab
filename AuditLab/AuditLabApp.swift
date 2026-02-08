08c08c443ce5ca2c05c46c787014005995769ff7
poorvarpatel <poorvarpatel@gmail.com>
2026-02-07 22:16:29 -0500

    Initial Commit

---
+++ AuditLab/AuditLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuditLabApp.swift
+//  AuditLab
+//
+//  Created by Poorva Patel on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AuditLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
